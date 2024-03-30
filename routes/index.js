957d185c97c450c31905fe671b1fdda825efe5bc
Anonymous <>
2024-03-30 14:39:03 -0700

    changed some stuff

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,35 @@ const { MongoClient } = require('mongodb');
 module.exports = {
     async saveSMSCode(req, res) {
         const uri = "mongodb+srv://michaeltwilliams92:RedLag00n1!2@@cluster0.gktdpbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-        const client = new MongoClient(uri);
-        let conn;
-        try {
-            conn = await client.connect();
-        } catch (e) {
-            console.error(e);
-        }
-        let db = conn.db("sms");
-        console.log('--------request-----------');
-        console.log(req);
-        let collection = await db("sms");
-        collection.updateOne({ phoneNumber: req.body.number }, { phoneNumber: req.body.number, message: req.body.message }, { upsert: true }, function (err, sms) {
-            if (err) {
-                return res.status(500);
-            }
-            return res.status(201);
-        });
+        // const client = new MongoClient(uri);
+        // let conn;
+        // try {
+        //     conn = await client.connect();
+        // } catch (e) {
+        //     console.error(e);
+        // }
+        // let db = conn.db("sms");
+        console.log('--------requestBody-----------');
+        console.log(req.body);
+        MongoClient.connect(uri, async (err, database) => {
+            const db = database.db('sms')
+            const collection = await db('sms')
+            collection.updateOne({ phoneNumber: req.body.number }, { phoneNumber: req.body.number, message: req.body.message }, { upsert: true }, function (err, sms) {
+                if (err) {
+                    return res.status(500);
+                }
+                return res.status(201);
+            });
+        })
+        // console.log('--------request-----------');
+        // console.log(req);
+        // let collection = await db("sms");
+        // collection.updateOne({ phoneNumber: req.body.number }, { phoneNumber: req.body.number, message: req.body.message }, { upsert: true }, function (err, sms) {
+        //     if (err) {
+        //         return res.status(500);
+        //     }
+        //     return res.status(201);
+        // });
 
     },
     async getSMSCode(req, res) {

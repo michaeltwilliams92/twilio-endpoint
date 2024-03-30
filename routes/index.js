74a29a1e291bb0902a257eeaceeb898d55f3c6fc
Anonymous <>
2024-03-30 14:21:54 -0700

    changed some stuff

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 
 module.exports = {
     async saveSMSCode(req, res) {
-        const uri = "mongodb+srv://michaeltwilliams92:RedLag00n1!2@3#@cluster0.gktdpbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+        const uri = "mongodb+srv://michaeltwilliams92:RedLag00n1!@cluster0.gktdpbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
         const client = new MongoClient(uri);
         let conn;
         try {

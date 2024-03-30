3f1d99968dd9ab8ec69c5dd362dc8d26e0e74b0e
Anonymous <>
2024-03-30 14:04:54 -0700

    changed some stuff

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,23 @@
-var db = require('../conn');
+import { collection as _collection } from '../conn';
 
-module.exports = {
-    async saveSMSCode(req, res) {
-        console.log('--------request-----------')
-        console.log(req)
-        let collection = await db.collection("sms");
-        collection.updateOne( { phoneNumber : req.body.number }, { phoneNumber : req.body.number, message : req.body.message }, { upsert : true }, function(err, sms) {
-            if (err) {
-                return res.status(500)
-              }
-              return res.status(201)
-        } );
+export async function saveSMSCode(req, res) {
+    console.log('--------request-----------');
+    console.log(req);
+    let collection = await _collection("sms");
+    collection.updateOne({ phoneNumber: req.body.number }, { phoneNumber: req.body.number, message: req.body.message }, { upsert: true }, function (err, sms) {
+        if (err) {
+            return res.status(500);
+        }
+        return res.status(201);
+    });
 
-    },
-
-    async getSMSCode(req, res) {
-        let collection = await db.collection("sms");
-        collection.findOne({ phoneNumber : req.body.number }, function(err, sms) {
-            if (err) {
-                return res.status(500).send({error: err})
-              }
-              return res.status(201).send({message: sms.message})
-        })
-    },
+}
+export async function getSMSCode(req, res) {
+    let collection = await _collection("sms");
+    collection.findOne({ phoneNumber: req.body.number }, function (err, sms) {
+        if (err) {
+            return res.status(500).send({ error: err });
+        }
+        return res.status(201).send({ message: sms.message });
+    });
 }

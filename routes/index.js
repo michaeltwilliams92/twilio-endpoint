0df85a20e393c836041c5b64797cb910ebc12921
Anonymous <>
2024-03-30 14:16:50 -0700

    changed some stuff

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,42 @@
 const { MongoClient } = require('mongodb');
+
 module.exports = {
     async saveSMSCode(req, res) {
-    const uri = "mongodb+srv://michaeltwilliams92:RedLag00n1!2@3#@cluster0.gktdpbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-    const client = new MongoClient(uri);
-    let conn;
-    try {
-    conn = await client.connect();
-    } catch(e) {
-    console.error(e);
-    }
-    let db = conn.db("sms");
-    console.log('--------request-----------');
-    console.log(req);
-    let collection = await db("sms");
-    collection.updateOne({ phoneNumber: req.body.number }, { phoneNumber: req.body.number, message: req.body.message }, { upsert: true }, function (err, sms) {
-        if (err) {
-            return res.status(500);
+        const uri = "mongodb+srv://michaeltwilliams92:RedLag00n1!2@3#@cluster0.gktdpbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+        const client = new MongoClient(uri);
+        let conn;
+        try {
+            conn = await client.connect();
+        } catch (e) {
+            console.error(e);
         }
-        return res.status(201);
-    });
+        let db = conn.db("sms");
+        console.log('--------request-----------');
+        console.log(req);
+        let collection = await db("sms");
+        collection.updateOne({ phoneNumber: req.body.number }, { phoneNumber: req.body.number, message: req.body.message }, { upsert: true }, function (err, sms) {
+            if (err) {
+                return res.status(500);
+            }
+            return res.status(201);
+        });
 
-},
-        async getSMSCode(req, res) {
-    let collection = await db("sms");
-    collection.findOne({ phoneNumber: req.body.number }, function (err, sms) {
-        if (err) {
-            return res.status(500).send({ error: err });
+    },
+    async getSMSCode(req, res) {
+        const uri = "mongodb+srv://michaeltwilliams92:RedLag00n1!2@3#@cluster0.gktdpbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+        const client = new MongoClient(uri);
+        let conn;
+        try {
+            conn = await client.connect();
+        } catch (e) {
+            console.error(e);
         }
-        return res.status(201).send({ message: sms.message });
-    });
-}
+        let collection = await db("sms");
+        collection.findOne({ phoneNumber: req.body.number }, function (err, sms) {
+            if (err) {
+                return res.status(500).send({ error: err });
+            }
+            return res.status(201).send({ message: sms.message });
+        });
+    }
 }

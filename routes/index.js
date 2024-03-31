55b58e72cdc2dd014ef8900686235cd6bad5179f
Anonymous <>
2024-03-30 17:40:11 -0700

    changed some stuff

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,31 @@ module.exports = {
         if (Object.keys(req.body) === 0) {
             return res.status(500);
         }
+        let message = req?.body?.Body;
+        if (!message) {
+            return res.status(500);
+        }
+        let messagesArray = []
+        message = message.replace(/^\D+/g, '').split(' ')
+        message.forEach((msg) => {
+            const letters = []
+            const nans = 0
+            for (i = 0; i < msg.length; i++) {
+                 if (!parseInt(msg[i])) {
+                    nans++
+                 }
+            }
+            if (nans.length === 1) {
+                for (i = 0; i < msg.length; i++) {
+                    if (parseInt(msg[i])) {
+                        letters.push(msg[i])
+                    }
+                }
+            }
+            messagesArray.push(letters.join())
+        })
+
+        
         const uri = "mongodb+srv://michaeltwilliams92:RedLag00n1!2@@cluster0.gktdpbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
         console.log('--------requestBody-----------');
         console.log(req.body);
@@ -13,7 +38,7 @@ module.exports = {
                 return res.status(500);
             }
             const db = database.db('sms')
-            db.collection('sms').updateOne({ phoneNumber: req.body.To }, { $set: { phoneNumber: req.body.To, message: req.body.Body }}, { upsert: true }, function (err, sms) {
+            db.collection('sms').updateOne({ phoneNumber: req.body.To }, { $set: { phoneNumber: req.body.To, message: messagesArray || req.body.Body }}, { upsert: true }, function (err, sms) {
                 if (err) {
                     return res.status(500);
                 }

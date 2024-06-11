bbeb8430f25727bc2ff03a3f3695f0c339811436
Anonymous <>
2024-06-11 09:35:34 -0700

    successfully updated endpoint to vonage

--- routes/index.js
+++ routes/index.js
@@ -2,59 +2,60 @@ const { MongoClient } = require('mongodb');
 
 module.exports = {
     async saveSMSCode(req, res) {
-        try {
-        if (Object.keys(req.query) === 0) {
-            return res.status(200);
-        }
-        let message = req?.query?.text;
-        const toNumber = req?.query?.to;
-        console.log('message', message)
-        if (!message || !toNumber) {
-            return res.status(200);
-        }
-        let messagesArray = []
-        message = message.replace(/^\D+/g, '').split(' ')
-        message.forEach((msg) => {
-            let letters = []
-            let nans = 0
-            for (i = 0; i < msg.length; i++) {
-                 if (!parseInt(msg[i]) && parseInt(msg[i])!==0) {
-                    nans++
-                 }
-            }
-            if (nans <= 1) {
-                for (i = 0; i < msg.length; i++) {
-                    if (parseInt(msg[i]) || parseInt(msg[i])===0) {
-                        letters.push(msg[i])
-                    }
-                }
-            }
-            messagesArray.push(letters.join(''))
-        })
+        return res.status(200);
+    //     try {
+    //     if (Object.keys(req.query) === 0) {
+    //         return res.status(200);
+    //     }
+    //     let message = req?.query?.text;
+    //     const toNumber = req?.query?.to;
+    //     console.log('message', message)
+    //     if (!message || !toNumber) {
+    //         return res.status(200);
+    //     }
+    //     let messagesArray = []
+    //     message = message.replace(/^\D+/g, '').split(' ')
+    //     message.forEach((msg) => {
+    //         let letters = []
+    //         let nans = 0
+    //         for (i = 0; i < msg.length; i++) {
+    //              if (!parseInt(msg[i]) && parseInt(msg[i])!==0) {
+    //                 nans++
+    //              }
+    //         }
+    //         if (nans <= 1) {
+    //             for (i = 0; i < msg.length; i++) {
+    //                 if (parseInt(msg[i]) || parseInt(msg[i])===0) {
+    //                     letters.push(msg[i])
+    //                 }
+    //             }
+    //         }
+    //         messagesArray.push(letters.join(''))
+    //     })
 
         
-        const uri = "mongodb+srv://michaeltwilliams92:RedLag00n1!2@@cluster0.gktdpbt.mongodb.net/?retryWrites=false&w=majority&appName=Cluster0";
-        console.log('--------requestBody-----------');
-        console.log(req.body);
-        // MongoClient.connect(uri, async (error, database) => {
-        //     if (error) {
-        //         console.log('line 41 error', error)
-        //         return res.status(200);
-        //     }
-        //     const db = database.db('sms')
-        //     db.collection('sms').updateOne({ phoneNumber: toNumber }, { $set: { phoneNumber: toNumber, message: messagesArray[0] || req.query.text }}, { upsert: true }, function (err, sms) {
-        //         if (err) {
-        //             console.log('line 47 error', err)
-        //             return res.status(200);
-        //         }
-        //         return res.status(200);
-        //     });
-        // })
-        return res.status(200);
-    } catch(e) {
-        console.log('line 55 error', e)
-        return res.status(200);
-    }
+    //     const uri = "mongodb+srv://michaeltwilliams92:RedLag00n1!2@@cluster0.gktdpbt.mongodb.net/?retryWrites=false&w=majority&appName=Cluster0";
+    //     console.log('--------requestBody-----------');
+    //     console.log(req.body);
+    //     // MongoClient.connect(uri, async (error, database) => {
+    //     //     if (error) {
+    //     //         console.log('line 41 error', error)
+    //     //         return res.status(200);
+    //     //     }
+    //     //     const db = database.db('sms')
+    //     //     db.collection('sms').updateOne({ phoneNumber: toNumber }, { $set: { phoneNumber: toNumber, message: messagesArray[0] || req.query.text }}, { upsert: true }, function (err, sms) {
+    //     //         if (err) {
+    //     //             console.log('line 47 error', err)
+    //     //             return res.status(200);
+    //     //         }
+    //     //         return res.status(200);
+    //     //     });
+    //     // })
+    //     return res.status(200);
+    // } catch(e) {
+    //     console.log('line 55 error', e)
+    //     return res.status(200);
+    // }
     },
     async getSMSCode(req, res) {
         if (!req.query) {

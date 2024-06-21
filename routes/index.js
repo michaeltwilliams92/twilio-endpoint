150ffda52efdd6d2f4a0f1222bb8f073d38b2d1c
Anonymous <>
2024-06-21 13:01:29 -0700

    testing

--- routes/index.js
+++ routes/index.js
@@ -61,7 +61,7 @@ module.exports = {
         if (!req.body) {
             return res.status(500);
         }
-        const uri = "mongodb+srv://michaeltwilliams92:RedLag00n1!2@@cluster0.gktdpbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+        const uri = "mongodb+srv://michaeltwilliams92:RedLag00n1!2@3#@cluster0.gktdpbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
         console.log('--------requestbody-----------');
         console.log(req.body);
         MongoClient.connect(uri, async (error, database) => {
